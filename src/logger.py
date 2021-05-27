555b127384c12186666d298c3d09741004d68d12
veliValentine <nikkevanukas1995@gmail.com>
2021-05-27 13:37:06 +0300

    feat: logger

---
+++ src/logger.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+LOG_FILE = "log.txt"
+
+class Logger:
+    def log(description, message = "", file_path = LOG_FILE):
+        file = None
+        try:
+            file = open(file_path, "a")
+            time = datetime.now()
+            log_entry = f"{time}, {description}, {message}\n"
+            file.write(log_entry)
+        except:
+            print("Error when writing to", file_path)
+        else:
+            print("Message saved to", file_path)
+        finally:
+            try:
+                file.close()
+            finally:
+                print(file_path, "closed")
